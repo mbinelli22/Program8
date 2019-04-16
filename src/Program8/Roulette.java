c51edc6879aa1a44877df8059af600413818b002
mbinelli22 <mbinelli1996@gmail.com>
2019-04-15 20:50:41 -0500

    First commit

---
+++ src/Program8/Roulette.java
@@ -0,0 +1,45 @@
+package Program8;
+//Michael Binelli
+import java.util.Scanner;
+
+public class Roulette {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+	}
+	
+	public static void welcome() {
+		
+	}
+	
+	public static int getMenuChoice() {
+		
+	}
+	
+	public static int getNumber(Scanner sdtIn) {
+		
+	}
+	
+	public static String getColor(Scanner stdIn) {
+		
+	}
+	
+	public static int getBet(Scanner stdIn, int chipsNow) {
+		
+	}
+	
+	public static int generateSpinResult()	{
+		
+	}
+	
+	public static String determineColor(int spinNum) {
+		
+	}
+	
+	public static void displaySpinResult(int spinNum) {
+		
+	}
+	
+	public static void finalReport(int chipsNow) {
+		
+	}
+}
